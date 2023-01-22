5c8b093cc444a657ed9ec271bedf4f75550cd198
Fukashu <57369434+Fukashi-Mati@users.noreply.github.com>
2023-01-22 23:47:43 +0100

    Dodanie komentarzy do kodu

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from pyod.models.knn import KNN
 import pandas as pd
 from kafka import KafkaConsumer
 
+
+# Pobranie danych uczących z pliku
 def getData():
     f = open("test.txt", "r")
     dane =[]
@@ -14,31 +16,40 @@ def getData():
 
 # Uczenie sztucznei inteligencji
 def learning(dane, pomiar):
+    # Ustawienie danych testowych w zbiór mozliwy do dostarczenia do nauki sztucznej inteligencji
     d = {'temp': dane, 'class': 0}
     learning_data = pd.DataFrame(data=d)
 
     # Normalizacja danych uczących
     x = learning_data['temp'].values.reshape(-1, 1)
 
-    #
+    # Określenie parametrów uczenia metodą najbliższych sąsiadów
     clf = KNN(contamination=0.03, n_neighbors=5)
+
+    # Uczenie sztucznej inteligencji na podstawie zbioru testowego
     clf.fit(x)
 
+    # Przypisanie danych z pomiaru do tablicy
     X_test = np.array([[pomiar]])
+
+    # Zwrócenie wyniku predykcji
     return clf.predict(X_test)
 
 
+# Pobranie danych uczących
 data = getData()
+# Pętla realizująca pobranie przetworzenie danych
 while True:
+    # Połączenie się z serwerem Kafki
     bootstrap_servers = ['192.168.0.103:9092']
     topicName = 'temp_fin2'
     consumer = KafkaConsumer(topicName, group_id='group1', bootstrap_servers=bootstrap_servers)
-    data = []
+    # Przetworzenie pobranych wiadomości
     for i in consumer:
+        # Zamiana wiadomości z binarnej na tekstową oraz usunięcie zbędznych znaków
         x = str(i.value)
         xsplit = x.split('--')
         temp = xsplit[1][:-1]
-        print(temp)
+        # Przekazanie temperatury do sprawdzenia, w przypadku wykrycia anomalii program wyświetli ją na ekranie
         if learning(data, float(temp[1])) == [1]:
-            print("Wykrto anomalię temperaturu o godzinie: ", xsplit[0])
-
+            print("Wykrto anomalię temperaturu o godzinie: ", xsplit[0], "temperatura: ", xsplit[1])
--- weather.py
+++ weather.py
@@ -3,25 +3,33 @@ from sense_emu import SenseHat
 from datetime import date
 import time
 import sys
-def getAndSendTemperature(sens):
 
+# Funkcja Pobierająca i wysyłająca temperaturę
+def getAndSendTemperature(sens):
+    # Pobranie temperatury oraz daty
     temperature = sens.temp
+    datawrite = date.now()
+
+    #Połączenie się z serwerm Kafki do którego wysyłamy dane
     KAFKA_HOST = "192.168.0.103:9092"
     client = KafkaClient(hosts=KAFKA_HOST)
     topic = client.topics["temp_fin"]
-    datawrite = date.now()
+
+    # Utworzenie wiadomości z odczytanej temperatury i daty oraz wysyłanie danych
     with topic.get_sync_producer() as producer:
         message = str(datawrite) + '--' + str(temperature)
         encoded_message = message.encode("utf-8")
         producer.produce(encoded_message)
 
 
+# Inicjacja czujników z symulatora
 sense = SenseHat()
+# Pobranie wartości próbkowania
 num = int(sys.argv[1])
 num2 = 0
+# Pętla realizująca pobór temperatury i realizująca próbkowanie
 while True:
     if num >= num2:
-
         num2 += 1
     else:
         getAndSendTemperature(sense)
